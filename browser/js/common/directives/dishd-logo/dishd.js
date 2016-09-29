74f412e5b6cfeb7aad0bdbc6834038d75057e96c
johanna08 <johannaperez@Johannas-MacBook.local>
2016-09-29 16:00:22 -0400

    logoupdated along with file names

--- browser/js/common/directives/dishd-logo/dishd.js
+++ browser/js/common/directives/dishd-logo/dishd.js
@@ -1,6 +1,6 @@
-app.directive('fullstackLogo', function () {
+app.directive('dishdLogo', function () {
     return {
         restrict: 'E',
-        templateUrl: 'js/common/directives/fullstack-logo/fullstack-logo.html'
+        templateUrl: 'js/common/directives/dishd-logo/dishd-logo.html'
     };
 });
