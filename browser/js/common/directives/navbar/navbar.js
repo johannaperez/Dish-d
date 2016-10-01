0eb4b0f7c4340930f0a621dfafc3392c21103762
johanna08 <johannaperez@JohannasMacBook.home>
2016-10-01 17:14:09 -0400

    prev commit footer view complete and navbar view now

--- browser/js/common/directives/navbar/navbar.js
+++ browser/js/common/directives/navbar/navbar.js
@@ -7,8 +7,6 @@ app.directive('navbar', function ($rootScope, AuthService, AUTH_EVENTS, $state) 
         link: function (scope) {
 
             scope.items = [
-                { label: 'Home', state: 'home' },
-                { label: 'About', state: 'about' },
                 { label: 'Members Only', state: 'membersOnly', auth: true }
             ];
 
