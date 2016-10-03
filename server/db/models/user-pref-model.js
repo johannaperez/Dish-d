1850fbe270d51fa580d84d93a0152f3eaf39011a
Dani YoungSmith <dyoungsmith@gmail.com>
2016-10-03 17:26:22 -0400

    Food categories (#26)
    * extracted food aisles
    
    * user prefs model complete
    
    * user prefs model updated
    
    * filter recipes by user prefs
    
    * associations added for user prefs

---
+++ server/db/models/user-pref-model.js
@@ -0,0 +1,52 @@
+// 'use strict';
+
+const Sequelize = require('sequelize');
+const db = require('../_db.js');
+const Recipe = require('./recipe-model.js');
+
+let UserPref = db.define('userPrefs', {
+	// SCHEMA
+	vegetarian: {
+		type: Sequelize.BOOLEAN,
+		defaultValue: false
+	},
+	vegan: {
+		type: Sequelize.BOOLEAN,
+		defaultValue: false
+	},
+	glutenFree: {
+		type: Sequelize.BOOLEAN,
+		defaultValue: false
+	},
+	dairyFree: {
+		type: Sequelize.BOOLEAN,
+		defaultValue: false
+	},
+	dislikes: {
+		type: Sequelize.ARRAY(Sequelize.STRING)
+	}
+}, {
+	// OPTIONS
+	instanceMethods: {
+		getAllOkayRecipes: () => {
+			return Recipe.findAll({
+				where: this
+			})
+			.then(boolRecipes => {
+				let filteredRecipes = [];
+				boolRecipes.forEach(recipe => {
+					recipe.extendedIngredients.forEach(ingredient => {
+						if (this.dislikes.includes(ingredient)) {
+							return;
+						}
+					})
+					filteredRecipes.push(recipe);
+				})
+				return filteredRecipes;
+			})
+		}
+	}
+});
+
+
+module.exports = UserPref
