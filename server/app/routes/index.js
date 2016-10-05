84a276894f3e1b56b1218fcc1b40c220f471ef90
Dani YoungSmith <dyoungsmith@gmail.com>
2016-10-05 13:55:12 -0400

    user prefs routes: create new, get one, update one (#36)

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -4,7 +4,7 @@ var router = require('express').Router(); // eslint-disable-line new-cap
 
 router.use('/members', require('./members'));
 router.use('/signup', require('./signup'));
-router.use('/api/preferences', require('./user-pref'));
+router.use('/users', require('./user'));
 
 // Make sure this is after all of
 // the registered routes!
@@ -13,5 +13,6 @@ router.use(function (req, res, next) {
     err.status = 404;
     next(err);
 });
+
 
 module.exports = router;
