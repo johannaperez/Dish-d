2fc9fa27b5c7187c711e0a1e744148c2e485ef32
johanna08 <johannaperez@JohannasMacBook.home>
2016-10-09 21:44:53 -0400

    background img settings completed

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -1,6 +1,22 @@
 app.config(function ($stateProvider) {
     $stateProvider.state('home', {
         url: '/',
-        templateUrl: 'js/home/home.html'
+        templateUrl: 'js/home/home.html',
+        controller: 'homeCtrl'
     });
 });
+
+app.controller('homeCtrl', ['$scope', function ($scope) {
+        $scope.resources = [
+            'http://techslides.com/demos/sample-videos/small.webm',
+            '*.ogv',
+            '*.mp4',
+            '*.swf'
+        ];
+        $scope.poster = 'http://placehold.it/2000&text=you%20may%20want%20to%20have%20a%20poster';
+        $scope.fullScreen = true;
+        $scope.muted = true;
+        $scope.zIndex = '10';
+        $scope.playInfo = {};
+        $scope.pausePlay = true;
+    }]);
