c3acf591e815a9afc6af2b152863d1c2c59f26cc
johanna08 <johannaperez@Johannas-MacBook.local>
2016-10-10 13:06:14 -0400

    background vid for home page complete

--- browser/js/app.js
+++ browser/js/app.js
@@ -1,5 +1,5 @@
 'use strict';
-window.app = angular.module('Dishd', ['fsaPreBuilt', 'ui.router', 'ngAnimate', 'ngMaterial', 'ngMdIcons', 'ngAria', 'slickCarousel', 'video-background']);
+window.app = angular.module('Dishd', ['fsaPreBuilt', 'ui.router', 'ngAnimate', 'ngMaterial', 'ngMdIcons', 'ngAria', 'slickCarousel', 'ngVidBg']);
 
 app.config(function ($urlRouterProvider, $locationProvider) {
     // This turns off hashbang urls (/#about) and changes it to something normal (/about)
--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -7,20 +7,16 @@ app.config(function ($stateProvider) {
 });
 
 app.controller('homeCtrl', ['$scope', function ($scope) {
-        $scope.myVideo = {
-            mp4: 'js/home/media/girls_cooking.mp4'
-        }
-
-        // [
-        //     '*.webm',
-        //     '*.ogv',
-        //     'http://ak0.picdn.net/shutterstock/videos/261040/preview/stock-footage-cooking-salad-sequence-vegetables-table-top.mp4',
-        //     '*.swf'
-        // ];
-        // $scope.poster = 'http://placehold.it/2000&text=you%20may%20want%20to%20have%20a%20poster';
-        // $scope.fullScreen = true;
-        // $scope.muted = true;
-        // $scope.zIndex = '10';
-        // $scope.playInfo = {};
-        // $scope.pausePlay = true;
+        $scope.resources = [
+            '*.webm',
+            '*.ogv',
+            'js/home/media/girls_cooking.mp4',
+            '*.swf'
+        ];
+        $scope.poster = 'http://placehold.it/2000&text=you%20may%20want%20to%20have%20a%20poster';
+        $scope.fullScreen = true;
+        $scope.muted = true;
+        $scope.zIndex = '10';
+        $scope.playInfo = {};
+        $scope.pausePlay = true;
     }]);
