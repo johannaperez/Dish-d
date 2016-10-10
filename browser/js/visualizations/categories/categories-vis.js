e40696ed788df75876aa201124029fd0d5fe0de7
Danielle YoungSmith <dyoungsmith@gmail.com>
2016-10-10 16:34:20 -0400

    sunburst and spending graphs are running

--- browser/js/visualizations/categories/categories-vis.js
+++ browser/js/visualizations/categories/categories-vis.js
@@ -1,42 +1,48 @@
 app.controller('CategoriesCtrl', ($scope) => {
 	$scope.options = {
-
+		chart: {
+			type: 'sunburstChart',
+			height: 300,
+			color: d3.scale.category20c(),
+			duration: 250
+		}
 	};
 
-	$scope.data = {
+	$scope.data = [{
 		name: 'I ate...',
 		children: [
-		{
-			name: 'Dairy',
-			children: [
-				{name: 'milk', size: number}
-			]
-		}, {
-			name: 'Meat, eggs, nuts',
-			children: [
-				{}
-			]
-		}, {
-			name: 'Fats, oils, sweets',
-			children: [
-				{}
-			]
-		}, {
-			name: 'Vegetables',
-			children: [
-				{}
-			]
-		}, {
-			name: 'Fruits',
-			children: [
-				{}
-			]
-		}, {
-			name: 'Carbs',
-			children: [
-				{}
-			]
-		}]
-	} // end of data
+			{
+				name: 'Dairy',
+				children: [
+					{name: 'milk', size: 5000}
+				]
+			}, {
+				name: 'Meat, eggs, nuts',
+				children: [
+					{name: 'eggs', size: 3000}
+				]
+			}, {
+				name: 'Fats, oils, sweets',
+				children: [
+					{name: 'donuts', size: 1000}
+				]
+			}, {
+				name: 'Vegetables',
+				children: [
+					{name: 'carrots', size: 8000}
+				]
+			}, {
+				name: 'Fruits',
+				children: [
+					{name: 'strawberries', size: 3000}
+				]
+			}, {
+				name: 'Carbs',
+				children: [
+					{name: 'tortelini', size: 9000}
+				]
+			}
+		]
+	}]; // end of data
 
 });
