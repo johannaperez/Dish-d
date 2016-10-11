b185bafa76f013d40867fa30beed09dd1f3fa03c
Danielle YoungSmith <dyoungsmith@gmail.com>
2016-10-11 16:58:40 -0400

    cluster chart on page

---
+++ browser/js/visualizations/overlap/overlap-vis.js
@@ -0,0 +1,88 @@
+app.controller('OverlapCtrl', ($scope) => {
+	// $scope.options = {
+	// 	chart: {
+	// 		type: 'bundle',
+	// 		size: [360, 360],
+	// 		sort: null
+	// 	}
+	// };
+
+	// let diameter = 600,
+	// 	radius = diameter / 2,
+	// 	innerRadius = radius - 120;
+
+	// let cluster = d3.layout.cluster()
+	// 	.size([300, innerRadius])
+	// 	.sort(null)
+	// 	.value(d => { return d.size });
+
+	// let bundle = d3.layout.bundle();
+
+	// let line = d3.svg.line.radial()
+	// 	.interpolate('bundle')
+	// 	.tension(.85)
+	// 	.radius(d => { return d.y })
+	// 	.angle(d => { return d.x / 180 * Math.PI });
+
+
+	// $scope.data = [{
+	// 	name: 'Recycled Ingredients',
+	// 	children: [
+	// 		{
+	// 			name: 'Baked falafel burger',
+	// 			children: [
+	// 				{name: 'fresh parsley', size: 3000},
+	// 				{name: 'salt', size: 3000},
+	// 				{name: 'cumin', size: 3000},
+	// 				{name: 'garlic', size: 3000},
+	// 				{name: 'lemon', size: 3000},
+	// 				{name: 'chickpeas', size: 3000},
+	// 				{name: 'chili garlic sauce', size: 3000},
+	// 				{name: 'oat flour', size: 3000},
+	// 				{name: 'tomato', size: 3000},
+	// 				{name: 'greens', size: 3000},
+	// 				{name: 'hummus', size: 3000},
+	// 			]
+	// 		}, {
+	// 			name: 'Kofta meatballs',
+	// 			children: [
+	// 				{name: 'fresh parsley', size: 3000},
+	// 				{name: 'salt', size: 3000},
+	// 				{name: 'cumin', size: 3000},
+	// 				{name: 'lemon', size: 3000},
+	// 				{name: 'cinnamon', size: 3000},
+	// 				{name: 'dried cherries', size: 3000},
+	// 				{name: 'allspice', size: 3000},
+	// 				{name: 'fresh mint', size: 3000},
+	// 				{name: 'cloves', size: 3000},
+	// 				{name: 'ground lamb', size: 3000},
+	// 				{name: 'honey', size: 3000},
+	// 				{name: 'olive oil', size: 3000},
+	// 				{name: 'pepper', size: 3000},
+	// 				{name: 'shallot', size: 3000},
+	// 				{name: 'sparkling water', size: 3000},
+	// 			]
+	// 		}, {
+	// 			name: 'Wild rice stuffing',
+	// 			children: [
+	// 				{name: 'fresh parsley', size: 3000},
+	// 				{name: 'salt', size: 3000},
+	// 				{name: 'garlic', size: 3000},
+	// 				{name: 'cinnamon', size: 3000},
+	// 				{name: 'dried cranberries', size: 3000},
+	// 				{name: 'bosc pear', size: 3000},
+	// 				{name: 'butternut squash', size: 3000},
+	// 				{name: 'celery', size: 3000},
+	// 				{name: 'coconut oil', size: 3000},
+	// 				{name: 'dried oregano', size: 3000},
+	// 				{name: 'dry white wine', size: 3000},
+	// 				{name: 'orange zest', size: 3000},
+	// 				{name: 'pecans', size: 3000},
+	// 				{name: 'wild rice', size: 3000},
+	// 				{name: 'yellow onion', size: 3000},
+	// 			]
+	// 		}
+	// 	]
+	// }];	// end of data
+
+});
