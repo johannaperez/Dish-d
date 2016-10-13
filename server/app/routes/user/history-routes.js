4ddb9fb55ab51417ce78620c7c05ecb24c8314a6
verasveras <lisaveras@gmail.com>
2016-10-13 16:59:09 -0400

    Route to get detailed meal plan history

--- server/app/routes/user/history-routes.js
+++ server/app/routes/user/history-routes.js
@@ -1,10 +1,11 @@
 const db = require('../../../db');
 const MealPlan = db.model('mealPlan');
 const User = db.model('user');
+const Recipe = db.model('recipe');
 const router = require('express').Router({mergeParams: true});
 const Promise = require('bluebird');
 
-
+// get none-detail history.
 router.get('', (req, res, next) => {
 
 	let id = req.params.userId;
@@ -17,6 +18,33 @@ router.get('', (req, res, next) => {
 	})
 	.then(function(mealPlans){
 		res.send(mealPlans);
+	})
+	.catch(next);
+
+});
+
+// get details of one meal plan.
+router.get('/:mealPlanId', (req, res, next) => {
+
+	let id = req.params.userId;
+	let mealPlanId = req.params.mealPlanId;
+
+	MealPlan.findOne({
+		where: {
+			userId: id,
+			id: mealPlanId,
+			status: 'complete'
+		}
+	})
+	.then(function(mealPlan){
+		return Recipe.findAll({
+			where: {
+				id: mealPlan.meals
+			}
+		})
+	})
+	.then(function(meals){
+		res.send(meals);
 	})
 	.catch(next);
 
